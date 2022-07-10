77a9df8a1e492e01983daadc24f82f11b3e6b41c
Matvey256 <97842332+MatveyKD@users.noreply.github.com>
2022-07-10 15:34:12 +0300

    Создал модели Pokemon и PokemonEntity
    1. Создал модель Pokemon
    2. Добавил метод __str__
    3. Добавил картинку
    4. Создал модель PokemonEntity
    5. Связал Pokemon и PokemonEntity
    6. Добавил поля появления и исчезновения PokemonEntity
    7. Добавил поля характеристик PokemonEntity
    8. Добавил поля описания и названий на англ/яп языках Pokemon
    9. Добавил эволюцию.
    10. Добавил verbose_name Pokemon
    11. Сделал поля необязательными Pokemon и PokemonEntity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,81 @@
-from django.db import models  # noqa F401
+from django.db import models
 
-# your models here
+class Pokemon(models.Model):
+    name = models.CharField(
+        max_length=20,
+        verbose_name="pokemon name"
+    )
+    image = models.ImageField(
+        verbose_name="pokemon image",
+        default="./media/default.jpg"
+    )
+    description = models.TextField(
+        blank=True,
+        verbose_name="pokemon description"
+    )
+    name_en = models.CharField(
+        max_length=20,
+        blank=True,
+        verbose_name="pokemon name_en"
+    )
+    name_jp = models.CharField(
+        max_length=20,
+        blank=True,
+        verbose_name="pokemon name_jp"
+    )
+
+    prev_evolution = models.ForeignKey(
+        "self",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="next_evolutions",
+        verbose_name="pokemon previous evolution"
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+class PokemonEntity(models.Model):
+    Pokemon = models.ForeignKey(
+        Pokemon,
+        on_delete=models.CASCADE,
+        verbose_name="pokemon"
+    )
+    Lat = models.FloatField(verbose_name="Latitute")
+    Lon = models.FloatField(verbose_name="Lontitute")
+
+    appeared_at = models.DateTimeField(
+        null=True,
+        verbose_name="appeared at"
+    )
+    disappeared_at = models.DateTimeField(
+        null=True,
+        verbose_name="disappeared at"
+    )
+
+    level = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="pokemon level"
+    )
+    health = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="pokemon health"
+    )
+    attack = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="pokemon attack"
+    )
+    deffend = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="pokemon defend"
+    )
+    endurance = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="pokemon endurance"
+    )
